7170bc9f6120afee3c9dbda97e9b6a1a671fce73
calebpenning <calebpenning@mail.com>
2021-06-19 21:11:58 -0700

    initial commit of my first react exercise... very basic component building and rendering

---
+++ part-1/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+    <div>
+        <FirstComponent />
+        <NamedComponent name="Caleb"/>
+    </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
